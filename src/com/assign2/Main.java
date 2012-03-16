243c1df8935e1b6e5a128573a3083dab0dec0886
Jason Recillo <jaysan1292@gmail.com>
2012-03-16 02:24:02 -0400

    init

---
+++ src/com/assign2/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.assign2;
+
+/**
+ *
+ * @author Jason Recillo
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
