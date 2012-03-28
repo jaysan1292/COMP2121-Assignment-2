fc035a198aef17934550b76de953e1aaec92559a
Jason Recillo <jaysan1292@gmail.com>
2012-03-28 12:55:26 -0400

    started data access classes

--- src/com/assign2/data/CommonAccess.java
+++ src/com/assign2/data/CommonAccess.java
@@ -4,6 +4,7 @@
  */
 package com.assign2.data;
 
+import com.assign2.Utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -17,7 +18,6 @@ public class CommonAccess {
     }
 
     public static Connection dbConnect() {
-
         try {
             String host = "jdbc:mysql://jaysan1292.com:3306/jrecillo_school";
             String user = "jrecillo";
@@ -25,7 +25,7 @@ public class CommonAccess {
             Connection conn = DriverManager.getConnection( host, user, pass );
             return conn;
         } catch ( SQLException ex ) {
-            //Utils.log_error( ex.getMessage() );
+            Utils.log_error( ex.getMessage() );
             return null;
         }
     }
--- src/com/assign2/data/CustomerAccess.java
+++ src/com/assign2/data/CustomerAccess.java
@@ -4,10 +4,51 @@
  */
 package com.assign2.data;
 
+import com.assign2.Utils;
+import com.assign2.business.Customer;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Jason Recillo
  */
 public class CustomerAccess {
-    
+    public static final String CUSTOMER_ID = "customer_id";
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String ADDRESS = "address";
+    public static final String PHONE_NUMBER = "phone_number";
+
+    public static Customer findCustomer( String column, String value ) {
+        try {
+            Utils.log_info( "Connecting to database..." );
+            Connection conn = CommonAccess.dbConnect();
+            Statement sqlStatement = conn.createStatement();
+            
+            String query = String.format( "select * from customer where %s='%s'", column, value );
+            Utils.log_info( "Executing SQL query: %s", query );
+            ResultSet resultSet = sqlStatement.executeQuery( query );
+
+            if ( !( resultSet.first() ) ) {
+                throw new SQLException( "Result set returned no data." );
+            }
+
+            Customer customer = new Customer();
+            customer.setCustomerId( resultSet.getInt( "customer_id" ) );
+            customer.setFirstName( resultSet.getString( "first_name" ) );
+            customer.setLastName( resultSet.getString( "last_name" ) );
+            customer.setAddress( resultSet.getString( "address" ) );
+            customer.setPhoneNumber( resultSet.getString( "phone_number" ) );
+
+            Utils.log_info( "Returned customer information:\n\t\tFirst name: %s\n\t\tLast name: %s", customer.getFirstName(), customer.getLastName() );
+
+            return customer;
+        } catch ( SQLException ex ) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 }
