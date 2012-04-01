ffec5e16311c7a75ff3c4c1880421d9d0635308b
Jason Recillo <jaysan1292@gmail.com>
2012-04-01 16:17:29 -0400

    housekeeping

---
+++ src/com/assign2/business/CustomerHandler.java
@@ -0,0 +1,58 @@
+package com.assign2.business;
+
+import com.assign2.Utils;
+import com.assign2.data.CustomerAccess;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author janjong
+ */
+public class CustomerHandler {
+
+    public void saveNewCustomer(Customer customer) {
+
+        Utils.log_info("Sending new customer information to database");
+
+        try {
+            CustomerAccess.addNewCustomer(customer.getFirstName(), customer.getLastName(), customer.getAddress(), customer.getPhoneNumber());
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+    }
+
+    public void UpdateExistingCustomer() {
+        Utils.log_info("Updating existing customer information");
+
+
+    }
+
+    public Customer findCusomter(String column, String _value) {
+        Customer _customer = new Customer();
+
+        try {
+            Utils.log_info("Searching for customer information");
+            _customer = CustomerAccess.findCustomer(column, _value);
+
+
+        } catch (SQLException ex) {
+
+            Logger.getLogger(Customer.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+        return _customer;
+    }
+
+    public void removeCustomer(int CId) {
+        try {
+            CustomerAccess.deleteCustomer(CId);
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
